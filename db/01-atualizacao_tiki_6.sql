0bd762e1dc75f04cebca860810d71c54009158ff
Rodrigo Sampaio Primo <rodrigo@utopia.org.br>
2011-10-18 19:30:43 -0200

    atualizacoes necessarias no banco de dados

---
+++ db/01-atualizacao_tiki_6.sql
@@ -0,0 +1,9 @@
+insert into tiki_preferences values ('log_sql', 'n') on duplicate key update value = 'n';
+delete from adodb_logsql;
+
+insert into tiki_preferences values ('change_language', 'y') on duplicate key update value = 'y';
+insert into tiki_preferences values ('feature_sitemycode', 'n') on duplicate key update value = 'n';
+insert into tiki_preferences values ('feature_sitelogo', 'n') on duplicate key update value = 'n';
+insert into tiki_preferences values ('feature_site_login', 'n') on duplicate key update value = 'n';
+insert into tiki_preferences values ('feature_tell_a_friend', 'n') on duplicate key update value = 'n';
+insert into tiki_preferences values( 'wikiplugin_banner' ,'y') on duplicate key update value = 'y';
