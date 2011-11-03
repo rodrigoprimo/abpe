a5e422488b0d7abc5dd7f4b72c95c3fedc8bd68d
Rodrigo Sampaio Primo <rodrigo@utopia.org.br>
2011-11-03 20:09:13 -0200

    remove do site blog, forum e agenda

--- db/01-atualizacao_tiki_6.sql
+++ db/01-atualizacao_tiki_6.sql
@@ -20,3 +20,12 @@ insert into tiki_preferences values ('site_title_location', 'before') on duplica
 insert into tiki_preferences values ('feature_search', 'y') on duplicate key update value = 'y';
 insert into tiki_preferences values ('feature_search_fulltext', 'y') on duplicate key update value = 'y';
 
+delete from tiki_menu_options where optionId = 190 and name = 'Agenda';
+delete from tiki_menu_options where optionId = 193 and name = 'Fórum';
+delete from tiki_menu_options where optionId = 192 and name = 'Blog da ABPE';
+
+insert into tiki_preferences values ('feature_forums', 'n') on duplicate key update value = 'n';
+insert into tiki_preferences values ('feature_blogs', 'n') on duplicate key update value = 'n';
+
+delete from `tiki_user_modules` where name = 'Menu_lateral';
+insert into `tiki_user_modules` (`name`,`title`,`data`, `parse`) VALUES ('Menu_lateral','Menu','{menu id=43}\r\n{if $user}\r\n  <div id=\"icone0\" class=\"option icone\">\r\n    <a class=\"linkmenu\" href=\"tiki-logout.php\">Sair</a>\r\n  </div>\r\n{else}\r\n  <div id=\"icone0\" class=\"option icone\">\r\n    <a class=\"linkmenu\" href=\"tiki-login.php\">Entrar</a>\r\n  </div>\r\n{/if}\r\n\r\n<center><a href=\"tiki-index.php?page=abpezinha\"><img src=\"styles/pe/ABPE_abpezinha.jpg\" alt=\"ABPEzinha\" title=\"ABPEzinha\" style=\"border:0px solid;\"></a><center>',NULL);
