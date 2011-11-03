548cb14e8a8cfcd5cd84c012b40d1c6fbe6e874e
Rodrigo Sampaio Primo <rodrigo@utopia.org.br>
2011-11-03 18:54:10 -0200

    remove referencias ao blog

--- src/gambi_nomes.php
+++ src/gambi_nomes.php
@@ -7,8 +7,6 @@ if (!preg_match('/tiki-read_article\.php/', $caminho)) {
 		$smarty->assign('nomepagina', $matches[1]);
 	} else if (preg_match('/tiki-view_articles\.php\?type\=(.*)/', $caminho, $matches)) {
 		$smarty->assign('nomepagina', $matches[1]);
-	} else if (preg_match('/tiki-view_blog.php/', $caminho) || preg_match('/tiki-view_blog_post.php/', $caminho)) {
-		$smarty->assign('nomepagina', 'blog');
 	} else if (preg_match('/tiki-forums.php/', $caminho) || preg_match('/tiki-view_forum.php/', $caminho) || preg_match('/tiki-view_forum_thread.php/', $caminho)) {
 		$smarty->assign('nomepagina', 'forum');
 	} else {
